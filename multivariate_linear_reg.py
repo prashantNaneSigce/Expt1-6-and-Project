276bed696f32b2d107bc33ba4b9eea5af3df7e16
prashantNaneSigce <115091025+prashantNaneSigce@users.noreply.github.com>
2022-10-05 19:40:40 +0530

    Add files via upload

---
+++ multivariate_linear_reg.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv('D:\\Sem 7\\ML\\EXPTS\\4\\homeprices.csv')
+
+md = df.bedrooms.median()
+df.bedrooms = df.bedrooms.fillna(md)
+
+reg = linear_model.LinearRegression()
+reg.fit(df.drop('price',axis='columns'),df.price)
+
+print("coef:", reg.coef_)
+print("intercept:", reg.intercept_)
+
+print("predict (area=3000, bedrooms=3, age=40):",reg.predict([[3000, 3, 40]]))
+
+print("predict (area=2500, bedrooms=4, age=5):",reg.predict([[2500, 4, 5]]))
